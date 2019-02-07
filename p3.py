f2273b120c2c577cc9987c64efd54dcb2e21763f
Eoin <oconne19@tcd.ie>
2019-02-07 01:32:18 +0000

    p3 now works fully

--- p3.py
+++ p3.py
@@ -2,9 +2,12 @@ import numpy as np
 from scipy import linalg
 
 
-def LU_solve(A,b):#The function takes a matrix A and a vector b and returns the vector x, the solution to Ax=b
+def null_col(A):#The function takes a matrix A and a vector b and returns the vector x, the solution to Ax=b
+    A_init = A
     r = 0 #This variable holds the current row that is being considered, goes from 0 to n-1
     c = 0
+    rank = 0
+    pivots = []
     size = min(A.shape) #the number of rows in the matrix
     rows = A.shape[0]
     cols = A.shape[1]
@@ -14,24 +17,29 @@ def LU_solve(A,b):#The function takes a matrix A and a vector b and returns the 
                 if A[i][r]!=0:
                     L_c = L_mat(r,i,rows,A[i][c]/A[r][c]) #this function calculates the L matrix for A[i][r]
                     A = np.dot(L_c,A) #apply the row operation to A
-                    b = np.dot(L_c,b) #apply the row operation to b
+            pivots.append(c)
             r+=1 #move on to next row
             c+=1
-            print (c)
         else: #if there is no pivot need to permute rows
             ind = first_nonzero(A[:,c],r,rows) #this function returns the index of the first non-zero entry below the pivot in that column
-            print (ind)
             if ind == -1: #if there is no non-zero entry then the matrix is singular
                 c+=1
             else:
                 P = perm_mat(ind,r,rows)# this function calculates the permutation matrix to swap row r with the first row below with non-zero pivot
                 A = np.dot(P,A) #apply the permutation the A
-                b = np.dot(P,b) #apply the permutation the b
-    #if abs(A[size-1][size-1])<10.0**(-10):#the final pivot of a singular matrix can often be non-zero due to rounding errors so if the final pivot is small enough claim that the matrix is singular
-        #return ("unavoidable zero pivot")
-    #x= back_subs(A,b,size)# solve Ux=c by back substitution
-    #return x
-    return A
+    out = []
+    if len(pivots) == cols:
+        out.append(np.zeros(cols))
+    else:
+        null_basis = np.zeros((cols,cols - len(pivots)))
+        for i in range(cols - len(pivots)):
+            null_basis[:,i] = back_subs_null(A,cols,rows,pivots,i)
+        out.append(null_basis)
+    col_space = np.zeros((rows,len(pivots)))
+    for i in range(len(pivots)):
+        col_space[:,i] = A_init[:,pivots[i]]
+    out.append(col_space)
+    return out
 
 
 def L_mat(r,i,rows,factor): #this creates the L matrix for the gi
@@ -53,27 +61,37 @@ def perm_mat(i,r,rows): #this creates the permuation matrix to swap rows i and j
     P[i][r] = 1
     return P
 
-def forward_subs(L,b,size): #solves Lx=b for x by forward substitution
-    x = np.zeros(size)
-    x[0] = b[0]/L[0][0]
-    for i in range(1,size):
-        sum = 0
-        for j in range(i):
-            sum += L[i][j]*x[j]
-        x[i] = (b[i]-sum)/L[i][i]
-    return x
 
-
-def back_subs(U,b,size): #solves Ux=b for x by backward substitution
-    x = np.zeros(size)
-    x[size-1] = b[size-1]/U[size-1][size-1]
-    for i in range(1,size):
-        sum = 0
-        for j in range(i):
-            sum += U[size-1-i][size-1-j]*x[size-1-j]
-        x[size-1-i] = (b[size-1-i]-sum)/U[size-1-i][size-1-i]
+def back_subs_null(U,cols,rows,pivots,count): #solves Ux=b for x by backward substitution
+    num_free = 0
+    piv = 1
+    x = np.zeros(cols)
+    for i in range(cols):
+        if pivots[-piv] == cols-i-1:
+            sum = 0
+            for j in range(i):
+                sum += U[rows-1-i+num_free,cols-1-j]*x[cols-1-j]
+            x[cols-i-1] = -float(sum)/U[rows-1-i+num_free,cols-1-i]
+            piv+=1
+        elif count == num_free:
+            x[cols-i-1] = 1
+            num_free+=1
+        else:
+            x[cols-i-1] == 0
+            num_free+=1
     return x
 
 A = np.array([[1.,6.,-3.,0.],[0.,4.,2.,-3.],[3.,18.,1.,-5.],[2.,0.,0.,3.],[2.,8.,2.,0.]])
-b = np.array([1,2,4,7,7])
-print (LU_solve(A,b))
+print ("Null Space: ")
+null,col = null_col(A)
+print (null)
+print ("Column Space: ")
+for i in range(col.shape[1]):
+    print (col[:,i])
+
+print ("Left Null Space: ")
+null,col = null_col(np.transpose(A))
+print (null)
+print ("Row Space: ")
+for i in range(col.shape[1]):
+    print (col[:,i])
